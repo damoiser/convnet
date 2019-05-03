daec641ff3fb796061032b278725cff75bc36a81
Damiano Radice <damiano.radice@inwork.ch>
2019-05-03 23:38:51 +0200

    begin with the steps

---
+++ steps.go
@@ -0,0 +1,26 @@
+package convnet
+
+// CreateStepConvolution create a convolutional step
+func CreateStepConvolution() *Step {
+
+}
+
+// CreateStepReLU apply the ReLU
+func CreateStepReLU() *Step {
+
+}
+
+// CreateStepPooling add the pooling
+func CreateStepPooling() *Step {
+
+}
+
+// CreateStepFlatten flat the volume to a matrix 1xN
+func CreateStepFlatten() *Step {
+
+}
+
+// CreateStepSoftmax add classification
+func CreateStepSoftmax() *Step {
+
+}
