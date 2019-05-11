e0f3d8a8cce824da523885d3b42a118fc84ee506
Damiano Radice <damiano.radice@inwork.ch>
2019-05-11 23:41:37 +0200

    add maxpool tests

--- matrix_test.go
+++ matrix_test.go
@@ -87,4 +87,75 @@ var _ = Describe("Matrix", func() {
 			Expect(res.content[0]).To(Equal([]float64{-1.0, 2.0, 3.0, 4.0, 5.0, -6.0, 7.0, 8.0, 9.0}))
 		})
 	})
+
+	Context("when testing maxpool", func() {
+		It("complains if filter_size is invalid", func() {
+			matrix := &Matrix{}
+			res, err := matrix.MaxPool(0, 2)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("filter_size is invalid"))
+			Expect(res).To(BeNil())
+		})
+
+		It("complains if stride is invalid", func() {
+			matrix := &Matrix{}
+			res, err := matrix.MaxPool(2, 0)
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("stride is invalid"))
+			Expect(res).To(BeNil())
+		})
+
+		It("calculates correctly with filter 2x2 and stride 2", func() {
+			matrix := &Matrix{
+				content: [][]float64{
+					[]float64{2.0, 3.0, 4.0, 0.0},
+					[]float64{1.0, 5.0, 3.0, 2.0},
+					[]float64{0.0, 4.0, 2.0, 3.0},
+					[]float64{1.0, 0.0, 6.0, 1.0},
+				},
+			}
+
+			res, err := matrix.MaxPool(2, 2)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res).NotTo(BeNil())
+			Expect(res.content[0]).To(Equal([]float64{5.0, 4.0}))
+			Expect(res.content[1]).To(Equal([]float64{4.0, 6.0}))
+		})
+
+		It("calculates correctly with filter 3x3 and stride 1", func() {
+			matrix := &Matrix{
+				content: [][]float64{
+					[]float64{2.0, 3.0, 4.0, 0.0},
+					[]float64{1.0, 5.0, 3.0, 2.0},
+					[]float64{0.0, 4.0, 2.0, 3.0},
+					[]float64{1.0, 0.0, 6.0, 1.0},
+				},
+			}
+
+			res, err := matrix.MaxPool(3, 1)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res).NotTo(BeNil())
+			Expect(res.content[0]).To(Equal([]float64{5.0, 5.0, 4.0, 3.0}))
+			Expect(res.content[0]).To(Equal([]float64{6.0, 6.0, 6.0, 3.0}))
+			Expect(res.content[0]).To(Equal([]float64{6.0, 6.0, 6.0, 3.0}))
+			Expect(res.content[0]).To(Equal([]float64{6.0, 6.0, 6.0, 1.0}))
+		})
+
+		It("calculates correctly with filter 3x3 and stride 2", func() {
+			matrix := &Matrix{
+				content: [][]float64{
+					[]float64{2.0, 3.0, 4.0, 0.0},
+					[]float64{1.0, 5.0, 3.0, 2.0},
+					[]float64{0.0, 4.0, 2.0, 3.0},
+					[]float64{1.0, 0.0, 6.0, 1.0},
+				},
+			}
+
+			res, err := matrix.MaxPool(3, 2)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res).NotTo(BeNil())
+			Expect(res.content[0]).To(Equal([]float64{5.0, 4.0}))
+			Expect(res.content[1]).To(Equal([]float64{6.0, 6.0}))
+		})
+	})
 })
