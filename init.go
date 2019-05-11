1628695659dd4df96503dde3a9cf7ce444357919
Damiano Radice <damiano.radice@inwork.ch>
2019-05-11 22:23:38 +0200

    fix

--- init.go
+++ init.go
@@ -1,20 +1,22 @@
 package convnet
 
 func New(input [][]int32) (*ConvNet, error) {
-  // do init
+	// do init
 
-  conv := &ConvNet{
-    steps: []*Steps{baseStep(input)},
-    input: input,
-  }
+	conv := &ConvNet{
+		steps: []*Step{baseStep(input)},
+		input: input,
+	}
 
-  return conv
+	return conv, nil
 }
 
 func (conv *ConvNet) Train() error {
-
+	// TODO
+	return nil
 }
 
 func (conv *ConvNet) Run() error {
-
+	// TODO
+	return nil
 }
